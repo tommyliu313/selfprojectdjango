468bf9f6044d9db829b3bf4717d6690038df0ffb
tommyliu313 <liuchaktin@gmail.com>
2024-09-17 14:47:11 +0800

    20240917 1447

--- transfer/models.py
+++ transfer/models.py
@@ -4,9 +4,13 @@ from warehouse.models import Warehouse
 # Create your models here.
 
 class transfer(models.Model):
+    transfer_id = models.IntegerField()
     user = models.ForeignKey(User,on_delete=models.DO_NOTHING)
     warehouse = models.ForeignKey(Warehouse, on_delete=models.DO_NOTHING)
     rent_date = models.DateTimeField()
     end_date = models.DateTimeField()
     rent_paid = models.FloatField()
     is_Active = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return self.transfer_id
