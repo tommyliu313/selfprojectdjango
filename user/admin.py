3afed5f36c881d0761f6de0c70758eb8d3f6ff56
tommyliu313 <liuchaktin@gmail.com>
2024-09-19 15:39:09 +0800

    20240919 1539

--- user/admin.py
+++ user/admin.py
@@ -3,10 +3,10 @@ from .models import User
 
 # Register your models here.
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('id','username','is_active', 'creation_date', 'email','phone','main_img','first_name','last_name')
+    list_display = ('id','username','is_active', 'creation_date', 'email','phone','first_name','last_name')
     list_display_links = ('id',)
     list_filter = ('username',)
-    list_editable = ('is_active','main_img','username','first_name','last_name')
+    list_editable = ('is_active','username','first_name','last_name')
     search_fields = ()
     list_per_page = 25
     
--- user/models.py
+++ user/models.py
@@ -7,7 +7,6 @@ class User(models.Model):
     first_name = models.CharField()
     last_name = models.CharField()
     creation_date = models.DateTimeField()
-    main_img = models.ImageField()
     email = models.EmailField(max_length=254)
     phone = models.CharField()
     is_active = models.BooleanField(default=True)
