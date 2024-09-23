4ff9ffb1e5355f785fd04d41adbe30aa99f4720a
tommyliu313 <liuchaktin@gmail.com>
2024-09-23 12:23:27 +0800

    20240923 1223

--- newprojecttrial/static/js/main.js
+++ newprojecttrial/static/js/main.js
@@ -7,3 +7,10 @@ function openeye(){
         valueofpassword.type= "password";
     }
 }
+
+$(window).scroll(function(e){
+    if ($(window).scrollTop()>=100 && $(window).scrollTop() !=0)
+       $("button.button.is-link.modal-button").removeClass("hidden");
+    else
+       $("button.button.is-link.modal-button").addClass("hidden");
+  });
