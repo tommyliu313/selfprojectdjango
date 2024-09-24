9cb76fdc07eeacd1e194362390442955942914fa
tommyliu313 <liuchaktin@gmail.com>
2024-09-24 17:25:17 +0800

    20240924 1725

--- pages/views.py
+++ pages/views.py
@@ -5,5 +5,8 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'pages/index.html')
 
+def about(request):
+    return render(request, 'pages/about.html')
+
 def district(request):
     pass
