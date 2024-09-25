268da6397d3baf2237f3b1ae8fed3d2c90a4750a
tommyliu313 <liuchaktin@gmail.com>
2024-09-25 11:52:21 +0800

    20240925 1152

--- user/views.py
+++ user/views.py
@@ -4,11 +4,13 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 # return to dashboard, show his information and record
+@login_required
 def dashboard(request):
     return render(request, 'user/dashboard.html')
 
-def login():
-    pass
 
-def logout():
-    pass
+def login(request):
+    return render(request, 'user/login.html')
+
+def logout(request):
+    return render(request, 'user/logout.html')
